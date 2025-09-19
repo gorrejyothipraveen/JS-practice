b6cd614bb946e2d2b6f83a8153ab52d03a0bfb5d
gorrejyothipraveen <jyothipraveengorre@gmail.com>
2025-09-19 16:54:54 +0530

    checking smallest number in given three numbers

--- testingFile.js
+++ testingFile.js
@@ -1,18 +1,18 @@
 let num1 = 1;
 let num2 = 2;
 let num3 = 3;
-let template = 0;
+let temp = 0;
 const range = 3;
-let largestNumber;
+let smallestNumber;
 for( let i = 0 ; i < range ; i++) {
-  template = num1;
+  templ = num1;
   num1 = num2;
   num2 = num3;
-  num3 = template;
-  largestNumber = (num1 > num2 && num1 > num3);
-  if ( largestNumber === true) {
-    largestNumber = num1;
+  num3 = temp;
+  if ((num1 < num2 && num1 < num3) === true) {
+    smallestNumber= num1;
     break;
   }
 }
-console.log(largestNumber);
+
+console.log("smallest number  :  ",smallestNumber);
