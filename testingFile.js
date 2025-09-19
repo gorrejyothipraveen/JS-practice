ef7104472d3c418b7cf0dbc30a4e80309bc7c18e
gorrejyothipraveen <jyothipraveengorre@gmail.com>
2025-09-19 14:04:20 +0530

    swaping of numbers multipletimes done by using for loop

--- testingFile.js
+++ testingFile.js
@@ -1,8 +1,12 @@
 let num1 = 1;
 let num2 = 2;
 let num3 = 3;
-const template = num1;
-num1 = num2;
-num2 = num3;
-num3 = template
-console.log(num1,num2,num3);
+const range = 3;
+let template;
+for( let i = 0 ; i < range ; i++) {
+  template = num1;
+  num1 = num2;
+  num2 = num3;
+  num3 = template;
+  console.log(num1,num2,num3);
+}
