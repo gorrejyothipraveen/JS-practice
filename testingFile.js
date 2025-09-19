f5a443e3c12b0eb3d4b91739f496e33673ff32a8
gorrejyothipraveen <jyothipraveengorre@gmail.com>
2025-09-19 16:48:27 +0530

    checking largest number in given three numbers

--- testingFile.js
+++ testingFile.js
@@ -1,12 +1,18 @@
 let num1 = 1;
 let num2 = 2;
 let num3 = 3;
+let template = 0;
 const range = 3;
-let template;
+let largestNumber;
 for( let i = 0 ; i < range ; i++) {
   template = num1;
   num1 = num2;
   num2 = num3;
   num3 = template;
-  console.log(num1,num2,num3);
+  largestNumber = (num1 > num2 && num1 > num3);
+  if ( largestNumber === true) {
+    largestNumber = num1;
+    break;
+  }
 }
+console.log(largestNumber);
