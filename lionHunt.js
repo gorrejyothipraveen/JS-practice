f28620ddfbbe8839c23f85e04c413b4f34278c47
gorrejyothipraveen <jyothipraveengorre@gmail.com>
2025-09-21 22:30:12 +0530

    finding the distance using if condition statement

--- lionHunt.js
+++ lionHunt.js
@@ -1,3 +1,29 @@
-const string = "LZ"
-const DistanceBetweenLandZ = string.length - 2;
+const string = "L  Z   "
+let positionOfLIon;
+if(string[0] === 'L') {
+  positionOfLIon = 0;
+}
+if(string[1] === 'L') {
+  positionOfLIon = 1;
+}
+if(string[2] === 'L') {
+  positionOfLIon = 2;
+}
+if(string[3] === 'L') {
+  positionOfLIon = 3;
+}
+let positionOfZebra;
+if(string[0] === 'Z') {
+  positionOfZebra = 0;
+}
+if(string[1] === 'Z') {
+  positionOfZebra = 1;
+}
+if(string[2] === 'Z') {
+  positionOfZebra = 2;
+}
+if(string[3] === 'Z') {
+  positionOfZebra = 3;
+}
+const DistanceBetweenLandZ = positionOfZebra - positionOfLIon - 1;
 console.log("the distance between",string,"is :",DistanceBetweenLandZ);
