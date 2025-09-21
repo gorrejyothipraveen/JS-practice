20f9bc0600ce9b01c43ed2c3233dbd07ee62919c
gorrejyothipraveen <jyothipraveengorre@gmail.com>
2025-09-21 22:31:59 +0530

    replacing if statement by loop statement

--- lionHunt.js
+++ lionHunt.js
@@ -1,29 +1,21 @@
-const string = "L  Z   "
-let positionOfLIon;
-if(string[0] === 'L') {
-  positionOfLIon = 0;
-}
-if(string[1] === 'L') {
-  positionOfLIon = 1;
-}
-if(string[2] === 'L') {
-  positionOfLIon = 2;
-}
-if(string[3] === 'L') {
-  positionOfLIon = 3;
-}
+const string = "Z  L"
+let positionOfLion;
 let positionOfZebra;
-if(string[0] === 'Z') {
-  positionOfZebra = 0;
+let i = 0;
+while ( i < string.length ) {
+  if( string[i] === 'L') {
+    positionOfLion = i;
+    console.log("lion",positionOfLion);
+  }
+  i = i+1;
 }
-if(string[1] === 'Z') {
-  positionOfZebra = 1;
+i = 0;
+while ( i < string.length ) {
+  if( string[i] === 'Z') {
+    positionOfZebra = i;
+    console.log("zebra",positionOfZebra);
+  }
+  i = i+1;
 }
-if(string[2] === 'Z') {
-  positionOfZebra = 2;
-}
-if(string[3] === 'Z') {
-  positionOfZebra = 3;
-}
-const DistanceBetweenLandZ = positionOfZebra - positionOfLIon - 1;
-console.log("the distance between",string,"is :",DistanceBetweenLandZ);
+const DistanceBetweenLandZ =( positionOfLion - positionOfZebra < 0 )? -(positionOfLion - positionOfZebra) : positionOfLion - positionOfZebra ;
+console.log("the distance between",string,"is :",DistanceBetweenLandZ - 1);
