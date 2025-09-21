2576441320c66540b8a4bd4791fb32bb90919622
gorrejyothipraveen <jyothipraveengorre@gmail.com>
2025-09-21 15:40:14 +0530

    final code for sum of natural numbers

--- sumOfNaturalNumbers.js
+++ sumOfNaturalNumbers.js
@@ -1,23 +1,7 @@
-let number = 5;
+let number = 4;
 const originalNumber = number;
 let sum = 0;
- if (number > 0) {
-  sum = sum + number;
-  number = number - 1;
-}
-if (number > 0) {
-  sum = sum + number;
-  number = number - 1;
-}
-if (number > 0) {
-  sum = sum + number;
-  number = number - 1;
-}
-if (number > 0) {
-  sum = sum + number;
-  number = number - 1;
-}
-if (number > 0) {
+while (number > 0) {
   sum = sum + number;
   number = number - 1;
 }
