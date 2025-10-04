df4c0481906c3a74365b58f2684d915ded499c91
gorrejyothipraveen <jyothipraveengorre@gmail.com>
2025-10-04 21:16:25 +0530

    Update 03_most_frequency_character.js

--- practice/03_most_frequency_character.js
+++ practice/03_most_frequency_character.js
@@ -1,5 +1,34 @@
+function countTheFrequency(sentence, letter, index, count) {
+
+  if (index === sentence.length) {
+    return count;
+  }
+
+  if (sentence[index] === letter) {
+    count++;
+  }
+
+  return countTheFrequency(sentence, letter, index + 1, count)
+}
+
+function findingMostOccurredCharacterInSentence(sentence, index, count, character) {
+
+  if (index === sentence.length) {
+    return character;
+  }
+
+  let mostOccurred = countTheFrequency(sentence, sentence[index], index + 1, count);
+   
+  if(mostOccurred > count) {
+    character = sentence[index];
+    count = mostOccurred;
+  }
+
+  return findingMostOccurredCharacterInSentence(sentence, index + 1, count, character);
+}
+
 function mostFrequenctCharacter(sentence) {
-  return (sentence[0]);
+  return findingMostOccurredCharacterInSentence(sentence, 0, 1, sentence[0]);
 }
 
 function workingTestCase(resultSymbol, description) {
@@ -38,6 +67,11 @@ function testMostFrequenctCharacter(description, sentence, expected) {
 function testAll() {
 
   testMostFrequenctCharacter("single letter", "a", "a");
+  testMostFrequenctCharacter("single letter", "ab", "a");
+  testMostFrequenctCharacter("letter repeats two times", "aba", "a");
+  testMostFrequenctCharacter("letter repeats two times", "abb", "b");
+  testMostFrequenctCharacter("letterrepeats 3 times ", "ababa", "a");
+
   
 }
 
