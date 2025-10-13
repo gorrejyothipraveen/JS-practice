e0d571d31d8c2ccdc8d1abceb61913dd69aabb8c
gorrejyothipraveen <jyothipraveengorre@gmail.com>
2025-10-13 21:44:31 +0530

    Update 05_guessing_word.js

--- games/05_guessing_word.js
+++ games/05_guessing_word.js
@@ -1,17 +1,25 @@
-function confirmation(word, guess) { 
+function playWordGuess(guess, word, clue, chances) {
+
   if (word === guess) {
     console.log("you won the match");
   } else {
-    console.log('you lost the game');
+    console.log(clue);
+    confirmation(word, clue, chances - 1)
+  }
+}
+
+function confirmation(word, clue, chances) { 
+  if (chances > 0) {
+    const guess = prompt("enter your guess\n");
+    playWordGuess(guess, word, clue, chances);
   }
 }
 
 function guessingWord() {
   const word = 'apple';
   const clue = 'if you eat my seeds upto 40 at a time you will die in 5 min\n';
-  console.log(clue);
-  const guess = prompt("enter your guess");
-  confirmation(word, guess);
+  const chances = 2;
+  confirmation(word, clue, chances);
 }
 
 guessingWord();
