202141b368a071fccffe96a4732c4404563625d9
gorrejyothipraveen <jyothipraveengorre@gmail.com>
2025-10-13 22:28:29 +0530

    Update 05_guessing_word.js

--- games/05_guessing_word.js
+++ games/05_guessing_word.js
@@ -1,25 +1,41 @@
-function playWordGuess(guess, word, clue, chances) {
-
-  if (word === guess) {
-    console.log("you won the match");
-  } else {
-    console.log(clue);
-    confirmation(word, clue, chances - 1)
-  }
+const WORD = 'apple';
+ 
+function underline(length) {
+  return '-'.repeat(length);
 }
 
-function confirmation(word, clue, chances) { 
-  if (chances > 0) {
-    const guess = prompt("enter your guess\n");
-    playWordGuess(guess, word, clue, chances);
+function playWordGuess(guess, clues, chances, clueIndex) {
+
+  if (WORD === guess) {
+    console.log("you won the match");
+  } 
+
+  if (chances > 1) {
+    console.log('\n'+ "clue :" + clues[clueIndex]);
+    console.log(underline(40));
   }
+  confirmation(clues, chances - 1, clueIndex + 1);
+}
+
+function confirmation(clues, chances, index) { 
+  if (chances > 0) {
+    const guess = prompt("enter your guess :  ");
+    playWordGuess(guess, clues, chances, index);
+    return;
+  }
+
+  console.log('\nbadluck chances are completed,you lost the game \n\nthe word is : ',WORD + '\n');
+  return;
 }
 
 function guessingWord() {
-  const word = 'apple';
-  const clue = 'if you eat my seeds upto 40 at a time you will die in 5 min\n';
-  const chances = 2;
-  confirmation(word, clue, chances);
+  const clues = ['this is fruit',
+     'I belongs to rose family', 
+     'it takes the energy of 50 leaves to produce one fruit',
+      'science of growing this fruit is pomology',
+       'this fruit contain 0% fat and no cholesterol'];
+  const chances = WORD.length;
+  confirmation(clues, chances, 0, 0);
 }
 
 guessingWord();
