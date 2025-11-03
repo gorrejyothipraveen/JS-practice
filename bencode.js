f661e1c03f50c74046216f4e919f7aca5f593326
gorrejyothipraveen <jyothipraveengorre@gmail.com>
2025-11-03 16:44:14 +0530

    Update bencode.js

--- bencode.js
+++ bencode.js
@@ -1,14 +1,48 @@
 function encodeDigitalData(integer) {
-  const encodedData = 'i' + integer + 'e';
-  return encodedData;
+  const encodedInteger = 'i' + integer + 'e';
+  return encodedInteger;
+}
+
+function encodeStringData(string) {
+  const stringSize = string.length;
+  const encodedString = stringSize + ':' + string;
+  return encodedString;
 }
 
 function encode(data) { 
   switch(typeof data) {
     case 'number':
       return encodeDigitalData(data);
+    case 'string':
+      return encodeStringData(data);
   }
 }
 
-const data = 'hello';
-console.log(encode(data));
+function testData(input, expected, description) {
+  const actual = encode(input);
+
+  if (actual === expected) {
+    console.log('✅', description);
+  }  else {
+    const symbol = '❌';
+    console.log(symbol , description, input, expected, actual);
+  }
+}
+
+function testIntegerNumbers() {
+  testData(1, 'i1e', 'single integer number');
+  testData(12, 'i12e', 'two digit integer number');
+  testData(123, 'i123e', 'three digit integer number');
+  testData(0, 'i0e', 'zero');
+}
+
+function testStrings() {
+  testData('a', '1:a', 'single letter');
+}
+
+function testAll() {
+  // testIntegerNumbers();
+  testStrings();
+}
+
+testAll();
