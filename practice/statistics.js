d42c387feb07268b5ba3e9febb803d601bf6aaf3
gorrejyothipraveen <jyothipraveengorre@gmail.com>
2025-11-04 22:09:58 +0530

    Update statistics.js

--- practice/statistics.js
+++ practice/statistics.js
@@ -1,4 +1,4 @@
-function mean(data) {
+function meanOf(data) {
   let sum = 0;
   for (let index = 0; index < data.length; index++) {
     sum = sum + data[index];
@@ -6,11 +6,52 @@ function mean(data) {
   return (sum / data.length);
 }
 
-function median(sortedList) {
-  const lowerIndex = Math.floor((sortedList.length - 1)/ 2);
-  const upperIndex = Math.ceil((sortedList.length - 1)/ 2);
+function medianOf(sortedList) {
+  const lowerIndex = Math.floor((sortedList.length - 1) / 2);
+  const upperIndex = Math.ceil((sortedList.length - 1) / 2);
 
-  return (sortedList[upperIndex] + sortedList[lowerIndex])/ 2;
+  return (sortedList[upperIndex] + sortedList[lowerIndex]) / 2;
+}
+
+function standardDeviationOf(data) {
+  let sum = 0;
+  const mean = meanOf(data);
+
+  for (let index = 0; index < data.length; index++) {
+    const difference = data[index] - mean;
+    console.log(difference);
+    sum = sum + Math.pow(difference, 2);
+  }
+
+  return Math.sqrt(sum / data.length);
+}
+
+function findFrequency(element, data) {
+  let frequency = 0;
+  for (let index = 0; index < data.length; index++) {
+    if (data[index] === element) {
+      frequency++;
+    }
+  }
+  return frequency;
+}
+
+function mode(data) {
+  let max = 0;
+  let position = -1;
+
+  for (let index = 0; index < data.length; index++) {
+    const count = findFrequency(data[index], data);
+    if (count > max) {
+      max = count;
+      position = index;
+    }
+  }
+
+  if (position !== -1) {
+    return data[position];
+  }
+  return position;
 }
 
 function sorting(data) {
@@ -20,7 +61,7 @@ function sorting(data) {
       if (sortedList[i] > sortedList[j]) {
         const temp = sortedList[j];
         sortedList[j] = sortedList[i];
-        sortedList[i] = temp; 
+        sortedList[i] = temp;
       }
     }
   }
@@ -28,6 +69,7 @@ function sorting(data) {
 }
 
 const data = [1, 2, 3, 4];
-console.log(mean(data));
+console.log(meanOf(data));
 console.log(sorting(data));
-console.log(median(sorting(data)));
+console.log(medianOf(sorting(data)));
+console.log(standardDeviationOf([0, 0, 0, 100]));
